b79e6f8997dd04ee5606ac8f57d81f85772f4cec
Christian Carter <christian@biggerboatconsulting.com>
2017-02-17 11:44:41 -0800

    Front page HTML mocked out

--- functional_tests.py
+++ functional_tests.py
@@ -41,7 +41,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Pick out a present for pup' for row in rows)
+            any(row.text == '1: Pick out a present for pup' for row in rows),
+            "New todo does not appear in the table."
         )
         # There is still a text box for adding another item
         self.fail('finish your test, puppy')
