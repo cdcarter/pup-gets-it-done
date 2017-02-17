e3da5919c875e2d07de727acaa19f7469f0ecbef
Christian Carter <christian@biggerboatconsulting.com>
2017-02-17 11:36:22 -0800

    refactor home_page to usea  template

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,7 @@
 """ Lists app views
 """
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home_page(request):
     """ The home_page view, which invites a user to engage """
-    return HttpResponse('<html><title>To-Do List</title></html>')
+    return render(request, 'home.html')
