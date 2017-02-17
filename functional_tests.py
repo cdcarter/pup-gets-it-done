ed9c9953551c79b9c10ce13b9d8f3f25333ec1fc
Christian Carter <christian@biggerboatconsulting.com>
2017-02-17 12:07:47 -0800

    refactor functional test to use helpers

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,17 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def _check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+    
+    def _type_and_submit_item(self, item_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(item_text)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
     def test_start_retrieve_list(self):
         """ Jason, a power user, can start a list
         and share it with a friend """
@@ -31,39 +42,18 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do'
         )
         # He types "Pick out a present for pup" into the box
-        inputbox.send_keys('Pick out a present for pup')
-
         # When he hits update, the page updates, and now the page
         # lists "1: Pick out a present for pup"
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: Pick out a present for pup',
-            [row.text for row in rows]
-        )
+        self._type_and_submit_item('Pick out a present for pup')
+        self._check_for_row_in_list_table('1: Pick out a present for pup')
 
         # There is still a text box for adding another item
-        inputbox = self.browser.find_element_by_id('id_new_item')
-
         # He enters "Order present for pup"
-        inputbox.send_keys('Order present for pup')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self._type_and_submit_item('Order present for pup')
 
         # The page updates again and shows two items on the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: Pick out a present for pup',
-            [row.text for row in rows]
-        )
-        self.assertIn(
-            '2: Order present for pup',
-            [row.text for row in rows]
-        )
+        self._check_for_row_in_list_table('1: Pick out a present for pup')
+        self._check_for_row_in_list_table('1: Order present for pup')
 
         # Jason can copy the link at the bottom of the page for this list.
         self.fail('finish tests')
