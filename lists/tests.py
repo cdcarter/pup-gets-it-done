5fc94b5c85f4a9802b0a6e47304225f18623c37a
Christian Carter <christian@biggerboatconsulting.com>
2017-02-19 23:44:21 -0800

    holy shit it works and has lists

--- lists/tests.py
+++ lists/tests.py
@@ -38,7 +38,10 @@ class ItemListModelTest(TestCase):
         self.assertEqual(saved_items.count(), 2)
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first item (ever) written')
+        self.assertEqual(
+            first_saved_item.text,
+            'The first item (ever) written'
+        )
         self.assertEqual(second_saved_item.text, second_item.text)
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.list, list_)
@@ -47,20 +50,26 @@ class ItemListModelTest(TestCase):
 class ListViewTest(TestCase):
     """ Unit tests for the List view """
 
-    def test_display_lists(self):
-        """ The home page table renders a row for each item """
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+    def test_display_only_items_in_list(self):
+        """ Getting a list should sho wonly objects in that list """
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
-        response = self.client.get('/lists/the-only-list/')
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
 
     def test_uses_list_template(self):
         """ Lists are rendered w/ the list template """
-        response = self.client.get('/lists/the-only-list/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_save_post_requests(self):
@@ -76,5 +85,42 @@ class ListViewTest(TestCase):
         response = self.client.post(
             '/lists/new', data={'item_text': 'A new list item'}
         )
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
 
-        self.assertRedirects(response, '/lists/the-only-list/')
+    def test_passes_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
+
+class NewItemTest(TestCase):
+    """ Tests for the new Item view """
+
+    def test_can_save_to_existing_list(self):
+        """ POSTing /lists/id/add_item adds an item! """
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        """ POSTing to /lists/id/add_item redirects to list view """
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 """ Lists app views
 """
 from django.shortcuts import render, redirect
-from lists.models import Item,List
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -10,18 +10,27 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
+def view_list(request, list_id):
     """ handle requests for the lists resource """
+    list_ = List.objects.get(id=list_id)
 
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_})
+
 
 def new_list(request):
-    """ Add a new item to the list """
+    """ Create a new list w/ an item """
 
     list_ = List.objects.create()
     Item.objects.create(
         text=request.POST.get('item_text', ''),
         list=list_
     )
-    return redirect('/lists/the-only-list/')
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    """ Add an item to an existing list """
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_id}/')
