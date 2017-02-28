2df6e756bcb5e2c4e6ea8c352266a96bcb78a388
Christian Carter <christian@biggerboatconsulting.com>
2017-02-27 20:10:31 -0800

    add bootstrap for sexy fonts

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 """ Functional tests for the Obey simple list app """
 import time
-from django.test import LiveServerTestCase
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,7 +10,7 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """ A Simple Visitor Test Flow """
     def setUp(self):
         self.browser = webdriver.Firefox()
